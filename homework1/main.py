b716f3c60606f727c29c2b2b590ec598abfe1924
pritakapoor <77901332+pritakapoor@users.noreply.github.com>
2021-02-04 15:29:28 -0600

    Add files via upload

---
+++ homework1/main.py
@@ -0,0 +1,16 @@
+print('Birthday Calculator')
+print('Current day')
+month = int(input('Month:'))
+day = int(input('Day:'))
+year = int(input('Year:'))
+print('Birthday')
+bmonth = int(input('Month:'))
+bday = int(input('Day:'))
+byear = int(input('Year:'))
+year1 = abs(year - byear)
+month1 = abs(month - bmonth)
+day1 = abs(day - bday)
+print('You are', year1, 'years old.')
+
+if (month == bmonth) and (day == bday):
+    print('Happy Birthday!')
