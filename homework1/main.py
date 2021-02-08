648fa54300087b36359bb095725afcf7308f2916
pritakapoor <77901332+pritakapoor@users.noreply.github.com>
2021-02-08 15:47:51 -0600

    Add files via upload

--- homework1/main.py
+++ homework1/main.py
@@ -1,17 +1,13 @@
-#Prita Kapoor 8010339
-print('Birthday Calculator')
-print('Current day')
-month = int(input('Month:'))
-day = int(input('Day:'))
-year = int(input('Year:'))
-print('Birthday')
-bmonth = int(input('Month:'))
-bday = int(input('Day:'))
-byear = int(input('Year:'))
-year1 = abs(year - byear)
-month1 = abs(month - bmonth)
-day1 = abs(day - bday)
-print('You are', year1, 'years old.')
-
-if (month == bmonth) and (day == bday):
-    print('Happy Birthday!')
+import math
+height = int(input('Enter wall height (feet):\n'))
+width = int(input('Enter wall width (feet):\n'))
+wall_area = height * width
+paint_needed = wall_area / 350
+cans = math.ceil(paint_needed)
+paint_colors_cost={'red':35,'blue':25,'green':23}
+print('Wall area: ' + str(wall_area) + ' square feet')
+print("Paint needed: {:.2f} gallons".format(paint_needed))
+print('Cans needed: ' + str(cans) + ' can(s)')
+color=input('\nChoose a color to paint the wall:\n')
+cost=cans * paint_colors_cost[color.lower()]
+print('Cost of purchasing ' + str(color) + ' paint: $' + str(cost))
