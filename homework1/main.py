96a593fec2021a8af50cc6faec938274db6cc3dd
pritakapoor <77901332+pritakapoor@users.noreply.github.com>
2021-02-08 16:03:25 -0600

    Add files via upload

--- homework1/main.py
+++ homework1/main.py
@@ -1,15 +1,3 @@
-#Prita Kapoor 8010339
-#3.18
-import math
-height = int(input('Enter wall height (feet):\n'))
-width = int(input('Enter wall width (feet):\n'))
-wall_area = height * width
-paint_needed = wall_area / 350
-cans = math.ceil(paint_needed)
-paint_colors_cost={'red':35,'blue':25,'green':23}
-print('Wall area: ' + str(wall_area) + ' square feet')
-print("Paint needed: {:.2f} gallons".format(paint_needed))
-print('Cans needed: ' + str(cans) + ' can(s)')
-color=input('\nChoose a color to paint the wall:\n')
-cost=cans * paint_colors_cost[color.lower()]
-print('Cost of purchasing ' + str(color) + ' paint: $' + str(cost))
+userNum = int(input())
+userNumSquared = userNum * userNum
+print(userNumSquared)
