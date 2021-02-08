8eb6406936d97b6aa367429e667b90d2f28ea5c6
pritakapoor <77901332+pritakapoor@users.noreply.github.com>
2021-02-08 16:17:37 -0600

    Update and rename main.py to 1.2.py

--- homework1/1.2.py
+++ homework1/1.2.py
@@ -1,16 +1,13 @@
+#Prita Kapoor
+#1.2
 user_num = int(input('Enter integer:\n'))
 print('You entered: ' + str(user_num))
-
 square_num = user_num * user_num
 cube_num = square_num * user_num
-
 print(str(user_num) + ' squared is ' + str(square_num))
 print('And ' + str(user_num) + ' cubed is ' + str(cube_num) + ' !!')
-
 user_num2 = int(input('Enter another integer:\n'))
-
 sum = user_num + user_num2
 product = user_num * user_num2
-
 print(str(user_num) + ' + ' + str(user_num2) + ' is ' + str(sum))
 print(str(user_num) + ' * ' + str(user_num2) + ' is ' + str(product))
